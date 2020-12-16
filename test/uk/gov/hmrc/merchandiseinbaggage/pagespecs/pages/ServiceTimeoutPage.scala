c2b80a2a8b5daecfc9589ad8c94b42212ee66858
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-12-16 12:18:04 +0000

    MIB-331: fix test

--- test/uk/gov/hmrc/merchandiseinbaggage/pagespecs/pages/ServiceTimeoutPage.scala
+++ test/uk/gov/hmrc/merchandiseinbaggage/pagespecs/pages/ServiceTimeoutPage.scala
@@ -19,8 +19,8 @@ package uk.gov.hmrc.merchandiseinbaggage.pagespecs.pages
 import org.openqa.selenium.WebDriver
 import uk.gov.hmrc.merchandiseinbaggage.BaseSpecWithApplication
 
-class SessionExpiredPage(implicit webDriver: WebDriver) extends BasePage
+class ServiceTimeoutPage(implicit webDriver: WebDriver) extends BasePage
 
-object SessionExpiredPage extends BaseSpecWithApplication {
-  val path = "/declare-commercial-goods/sign-out"
+object ServiceTimeoutPage extends BaseSpecWithApplication {
+  val path = "/declare-commercial-goods/service-timed-out"
 }
